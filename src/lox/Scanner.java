123c05bd4388f62610fbe17041881a71810f46e0
MarioZurera <marioza2004@gmail.com>
2024-09-09 18:10:50 +0200

    refactor: remove unused import

--- src/lox/Scanner.java
+++ src/lox/Scanner.java
@@ -5,8 +5,6 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-import lox.TokenType;
-
 public class Scanner {
     private final String source;
     private final List<Token> tokens = new ArrayList<>();

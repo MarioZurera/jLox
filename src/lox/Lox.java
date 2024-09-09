87c220fd62dff2eb97fc85e6b0f889d4a0355deb
MarioZurera <marioza2004@gmail.com>
2024-09-09 17:36:27 +0200

    fix: unuse import

--- src/lox/Lox.java
+++ src/lox/Lox.java
@@ -1,8 +1,7 @@
-package Lox;
+package lox;
 
 import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.nio.charset.Charset;
 import java.nio.file.Files;

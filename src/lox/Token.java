8ba34ca30963a2bbb7d46c718a69c6dacd6e1c30
mzurera- <mzurera-@student.42malaga.com>
2024-09-11 13:37:39 +0200

    feat: hide Token attr

--- src/lox/Token.java
+++ src/lox/Token.java
@@ -1,26 +1,42 @@
 package lox;
 
 public class Token {
-    final TokenType type;
-    final String lexeme;
-    final Object literal;
-    final int line;
+    private final TokenType _type;
+    private final String _lexeme;
+    private final Object _literal;
+    private final int _line;
 
     Token(TokenType type, String lexeme, Object literal, int line) {
-        this.type = type;
-        this.lexeme = lexeme;
-        this.literal = literal;
-        this.line = line;
+        this._type = type;
+        this._lexeme = lexeme;
+        this._literal = literal;
+        this._line = line;
+    }
+
+    public TokenType getType() {
+        return _type;
+    }
+
+    public String getLexeme() {
+        return _lexeme;
+    }
+
+    public Object getLiteral() {
+        return _literal;
+    }
+
+    public int getLine() {
+        return _line;
     }
 
     @Override
     public String toString() {
-        String typeText = type.toString();
+        String typeText = _type.toString();
         if (typeText.length() < 4)
             typeText += "\t";
         if (typeText.length() < 8)
             typeText += "\t";
-        String value = literal != null ? literal.toString() : lexeme;
+        String value = _literal != null ? _literal.toString() : _lexeme;
         return typeText + "\t\t" + value;
     }
 }
